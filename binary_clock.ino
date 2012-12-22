eff20fd6cc0294be7cea998238601076567c667f
Antonis Kalou <slacker@mint-slacker.(none)>
2012-12-22 12:30:51 +0200

    Changed code completelly, fully working now

--- binary_clock.ino
+++ binary_clock.ino
@@ -1,107 +1,190 @@
 /* Arduino binary clock*
+
  ************************************************************************************
  * Use instructions                                                                 *
- *######################                                                            *
+ * ######################                                                           *
  * Connect LED in consecutive order                                                 *
- * So ---> Minute LED's start from pin 1 and end at 6, 1 being the binary value of 32,*
- *         2 being the binary value of 16                                           *
- *          etc.                                                                    *
- *                                                                                  *    
- *         Hour LED's start from pin 7 and end at 10, 7 being the binary value of 8 *
+ * --> Minute LED's start from PIN2 and end at 7, PIN2 being the binary value of 1, *
+ *         PIN3 being the binary value of 2 etc.                                    *
+ *     Hour LED's start from pin 8 and end at 12, 8 being the binary value of 1.	*
+ *																					*
+ *																					*
+ *   IMPORTANT: set 'minutes' and 'hours' to the appropiate values                  *
  *                                                                                  *
  ************************************************************************************
  *                                                                                  *
- *      Developed by Antonis Kalou (kalouantonis@gmail.com) under the GPL v3 Licence*
+ *                                                                                  *                                                                  
+ *  Developed by Antonis Kalou (kalouantonis@gmail.com) and Anthony Clays           *                    
+ *   under the GPL v3 Licence                                                       *
  ************************************************************************************
  */
 
+ 
+
+int minutes = 0;
+
+int hours = 0;
+
+ 
+
+unsigned long lastUpdate; // variables used to correct minor timing errors
+
+unsigned long waitTime;
+
+const unsigned long M60 = 3000000; // 60 million microsenconds = 1 minute
+
+ 
+
 void setup()
+
 {
+
   /* Initialise pins -- set to output*/
-  int i;
-  
-  Serial.begin(9600); /* Initialise serial port*/
-  
-  for(i = 0; i < 10; i++)
+
+  for(int i = 2; i < 13; i++){
+
     pinMode(i, OUTPUT);
-}
 
-int minute_loop(int minutes)
-{
- /* Move minutes to corresponding LED Pin(s) */ 
-  int i;
-  int j = 0;
- for(i = 32; j < 6; i/2)
- { 
-   int curr_led = 6 - j;
-   if(minutes == 0)
-     break;
-   if(minutes >= i)
-    {
-      minutes -= i; digitalWrite(curr_led, HIGH);
-    }
-    else
-      digitalWrite(curr_led, LOW);
-    j++;
-    
-  }  
-    
-  return minutes; /* Check that minutes is 0*/
-}
+  }
 
-int hour_loop(int hours)
-{
- /* Move hours to corresponding LED pin(s) */ 
- int i;
- int j = 0;
- for(i = 8; j < 4; i/2)
- { 
-   int curr_led = 10 - j;
-   if(hours == 0)
-     break;
-   if(hours >= i)
-    {
-      hours -= i; digitalWrite(curr_led, HIGH);
-    }
-    else
-      digitalWrite(curr_led, LOW);
-    j++;
-    
-  }  
-  return hours; /* Check that hours is 0*/
   
+
+  //Serial.begin(9600); /* Initialise serial port*/
+
+  
+
+  minutes--;
+
 }
+
+ 
 
 void loop()
+
 {
-  int minutes = 0;
-  int hours = 0;
-  Serial.print("Enter minutes\n");
-  minutes = Serial.read();
-  Serial.print("Enter hours\n");
-  hours = Serial.read();
+
+  // minute loop
+
+  lastUpdate = micros();
+
   
-  while(((minutes <= 60) || (minutes >= 0)) || ((hours <= 24) || (hours >= 0))) /* Remove while loop, input only run once*/
+
+  minutes++;
+
+  // hour loop, olny if necessary
+
+  if(minutes > 59)
+
   {
-    int minute_val = minute_loop(minutes);
-    int hour_val = hour_loop(hours);
+
+    minutes = 0;
+
+    for(int i = 2; i < 8; i++)
+
+      digitalWrite(i, LOW);
+
     
-    if((minute_val != 0) || (hour_val != 0)) /* Check validation of data*/
-    { 
-      Serial.print("Error in minute or hour calculation: Return value not 0\n");
-      exit(1);
-    }
-    
-    delay(6000); /* Wait one minute*/ 
-    minutes++;
-    if(minutes == 60)
-      hours++; minutes = 0;
-    if(hours == 24)
+
+    if(++hours == 24)
+
+    {
+
       hours = 0;
+
+      for(int j = 8; j < 13; j++)
+
+        digitalWrite(j, LOW);
+
+    }
+
+    else
+      hour_loop(hours);
+
   }
-  Serial.print("Invalid input: You must enter minutes between 0 and 60 and hours between 0 and 24\n");
+
+  else
+    minute_loop(minutes);
+
   
+
+  //determine how long to wait before next loop.
+
+  if(lastUpdate > micros()) // micros() loops back to 0 after 70 mins or so
+
+    lastUpdate = 0;
+
+  
+
+  waitTime = M60 - micros() + lastUpdate;
+  
+
+  delay(waitTime / 1000);
+
 }
 
+ 
 
+void minute_loop(int mins)
 
+{
+
+  /* Move minutes to corresponding LED Pin(s) */
+
+  
+
+  boolean current; // should the current pin be ON?
+
+  
+
+  for(int i = 2; i < 8; i++)
+
+  {
+
+    current = ((mins % 2) == 1);
+
+    
+
+    digitalWrite(i, current);
+
+    if(current)
+      mins--;
+    if(mins == 0)
+      break;
+    mins /= 2;
+
+  }
+
+}
+
+ 
+
+void hour_loop(int hs)
+
+{
+
+  /* Move hours to corresponding LED pin(s) */
+
+  
+
+  boolean current;
+
+  
+
+  for(int i = 8; i < 13; i++)
+
+  {
+    current = ((hs % 2) == 1);
+    digitalWrite(i, current);
+    
+
+    if(current)
+      hs--;
+
+    if(hs == 0)
+      break;
+
+    hs /= 2;
+
+  }
+
+}
